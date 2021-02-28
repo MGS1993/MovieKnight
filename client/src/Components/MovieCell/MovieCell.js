6a7add48e2a95d7129acd1f6bbe818a1f7457ee9
Manuel Guzman <m_guzman74@yahoo.com>
2021-02-28 11:42:47 -0600

    added expand/contract moviecellwrapper on click

--- client/src/Components/MovieCell/MovieCell.js
+++ client/src/Components/MovieCell/MovieCell.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import styles from './MovieCell.module.css'
+import React, { useState } from "react";
+import styles from "./MovieCell.module.css";
+const MovieCell = (props) => {
+  let [clickedLarger, setClickedLarger] = useState(false);
+  let appendedStyle;
 
-const MovieCell = props => {
+  clickedLarger
+    ? (appendedStyle = { height: "500px" })
+    : (appendedStyle = null);
 
-  return(
-    <div className={styles.movieCellWrapper}>
+  return (
+    <div
+      style={appendedStyle}
+      className={styles.movieCellWrapper}
+      onClick={() => setClickedLarger(!clickedLarger)}
+    >
       <div className={styles.movieImageWrapper}>
-        <img className={styles.movieImage} src={props.image} alt={`Movie Poster for ${props.title}`}></img>
+        <img
+          className={styles.movieImage}
+          src={props.image}
+          alt={`Movie Poster for ${props.title}`}
+        ></img>
       </div>
       <div className={styles.movieInfoWrapper}>
         <div className={styles.title}>
           <div>{props.title}</div>
           <div className={styles.titleScore}>{props.score}</div>
         </div>
-        <div className={styles.bio}>
-          {props.bio}
-        </div>
+        <div className={styles.bio}>{props.bio}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default MovieCell
+export default MovieCell;
