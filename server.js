e98019c4cac096e4648605345e0632a7129adec6
Manuel Guzman <m_guzman74@yahoo.com>
2021-03-08 19:23:03 -0600

    prep for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
 require('dotenv').config();
 
 
@@ -11,12 +12,13 @@ app.use(cors());
 // const mongoose = require('mongoose');
 // const uri = process.env.ATLAS_URI;
 
+// Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
+
   app.get('*', () => (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
   });
 }
 
