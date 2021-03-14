665e9a84d54873d7dccb745a3a2789af5e533614
Manuel Guzman <m_guzman74@yahoo.com>
2021-03-14 07:28:33 -0600

    moved navSearch functionality to FooterNavBar for easier pageview impl

--- client/src/Components/FooterNavBar/FooterNavBar.js
+++ client/src/Components/FooterNavBar/FooterNavBar.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import styles from './FooterNavBar.module.css';
 import { GoSearch } from "react-icons/go";
 import { IoIosArrowDown } from "react-icons/io";
+import ListContext from '../context/listContext';
 import NavArrow from '../NavSearch/NavArrow/NavArrow';
 import NavSearch from '../NavSearch/NavSearch';
 const FooterNavBar = props => {
   const [expandedNav, setExpandedNav] = useState(false)
   const [genreList, setGenreList] = useState('');
-  
+  const listContext = useContext(ListContext)
+  const [ mediaNav, setMediaNav ] = useState('root')
+  const [ tvGenreList, setTvGenreList ] = useState('')
   let expandedStyle = null;
   let icon = <GoSearch color="orange" />
   
@@ -19,7 +22,52 @@ const FooterNavBar = props => {
     }
     icon = <IoIosArrowDown color="orange" />
   }
+  const queryMediaBySelectedGenre = async (e, mediaType, voteCount, page) => {
 
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/discover/${mediaType}?api_key=${props.apiKey}&language=en-US&sort_by=vote_average.desc&vote_count.gte=${voteCount}&with_genres=${e.value}&include_adult=false&include_video=false&page=${page}&watch_region=US`)
+        const data = await response.json();
+        console.log(data)
+        listContext.exportedData(data.results)
+        setExpandedNav(!expandedNav)
+        setMediaNav('root')
+    }catch(err) {
+        console.log(err)
+    }
+}
+const queryTrendingMedia = async (mediaType) => {
+  try{   
+      const response = await fetch(` https://api.themoviedb.org/3/trending/${mediaType}/week?api_key=${props.apiKey}`)
+      const data = await response.json();
+      listContext.exportedData(data.results)
+      setExpandedNav(!expandedNav)
+      setMediaNav('root')
+  }catch(err) {
+      console.log(err)
+  }
+}
+const queryTopMediaAllGenres = async (mediaType, voteCount) => {
+  try {
+      const response = await fetch(`https://api.themoviedb.org/3/discover/${mediaType}?api_key=${props.apiKey}&language=en-US&sort_by=vote_average.desc&vote_count.gte=${voteCount}&page=1&timezone=America%2FTexas&include_null_first_air_dates=false`)
+      const data = await response.json();
+      listContext.exportedData(data.results)
+      setExpandedNav(!expandedNav)
+      setMediaNav('root')
+  }catch(err) {
+      console.log(err)
+  }
+}
+const callApiForTvGenre = async (e) => {
+  setMediaNav('topTvShows')
+  try{
+      const responseGenre = await fetch(`https://api.themoviedb.org/3/genre/tv/list?api_key=${props.apiKey}&language=en-US`)
+      const dataGenre = await responseGenre.json();
+      const genreListQuery = dataGenre.genres;
+      setTvGenreList(genreListQuery);
+  }catch(err) {
+      console.log(err)
+  }
+}
   useEffect(() => {
     const queryData = async() => {
       try {
@@ -33,20 +81,24 @@ const FooterNavBar = props => {
      queryData()
   }, [props.apiKey])
 
-
-   
   return(
     <div style={expandedStyle} className={styles.navBarWrapper}>
 
       <NavArrow arrowDir='left' apiKey={props.apiKey} setExpandedNav={setExpandedNav} />
 
       <NavSearch expandedNav={expandedNav} setExpandedNav={setExpandedNav} 
-        genreList={genreList} apiKey={props.apiKey} />
+        genreList={genreList} apiKey={props.apiKey}
+        mediaNav={mediaNav}
+        setMediaNav={setMediaNav}
+        queryMediaBySelectedGenre={queryMediaBySelectedGenre}
+        queryTrendingMedia={queryTrendingMedia}
+        queryTopMediaAllGenres={queryTopMediaAllGenres}
+        callApiForTvGenre={callApiForTvGenre}
+        tvGenreList={tvGenreList} />
 
       <div onClick={() => setExpandedNav(!expandedNav)} 
         className={styles.searchWrapper}> {icon} </div>
         <NavArrow arrowDir='right' apiKey={props.apiKey} setExpandedNav={setExpandedNav} />
-      
       
     </div>
   )
--- client/src/Components/NavSearch/NavSearch.js
+++ client/src/Components/NavSearch/NavSearch.js
@@ -1,14 +1,9 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 import styles from './NavSearch.module.css';
-import ListContext from '../context/listContext';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
 const NavSearch = props => {
-    const listContext = useContext(ListContext)
-    const [ mediaNav, setMediaNav ] = useState('root')
-    const [ tvGenreList, setTvGenreList ] = useState('')
-    // const [pageNavIO, setPageNavIO] = useState(false);
     let expandedStyle = null 
     let genreListNames = []
     let tvGenreListNames = []
@@ -22,8 +17,8 @@ const NavSearch = props => {
             })
         })
     }
-    if(tvGenreList !== '') {
-        tvGenreList.forEach(el => {
+    if(props.tvGenreList !== '') {
+        props.tvGenreList.forEach(el => {
             tvGenreListNames.push({
                 value: el.id,
                 label: el.name
@@ -37,71 +32,71 @@ const NavSearch = props => {
          transition: '.5s ease-in-out'
         }
     }
-    const queryTrendingMedia = async (mediaType) => {
-        try{   
-            const response = await fetch(` https://api.themoviedb.org/3/trending/${mediaType}/week?api_key=${props.apiKey}`)
-            const data = await response.json();
-            listContext.exportedData(data.results)
-            props.setExpandedNav(!props.expandedNav)
-            setMediaNav('root')
-        }catch(err) {
-            console.log(err)
-        }
-    }
-    const queryMediaBySelectedGenre = async (e, mediaType, voteCount, page) => {
-        // setPageNavIO(!pageNavIO)
-        let test = `https://api.themoviedb.org/3/discover/${mediaType}?api_key=${props.apiKey}&language=en-US&sort_by=vote_average.desc&vote_count.gte=${voteCount}&with_genres=${e.value}&include_adult=false&include_video=false&page=${page}&watch_region=US`
-        try {
-            const response = await fetch(test)
-            const data = await response.json();
-            console.log(data)
-            console.log(test)
-            listContext.exportedData(data.results)
-            props.setExpandedNav(!props.expandedNav)
-            setMediaNav('root')
-        }catch(err) {
-            console.log(err)
-        }
-    }
-    const queryTopMediaAllGenres = async (mediaType, voteCount) => {
-        try {
-            const response = await fetch(`https://api.themoviedb.org/3/discover/${mediaType}?api_key=${props.apiKey}&language=en-US&sort_by=vote_average.desc&vote_count.gte=${voteCount}&page=1&timezone=America%2FTexas&include_null_first_air_dates=false`)
-            const data = await response.json();
-            listContext.exportedData(data.results)
-            props.setExpandedNav(!props.expandedNav)
-            setMediaNav('root')
-        }catch(err) {
-            console.log(err)
-        }
-    }
+    // const queryTrendingMedia = async (mediaType) => {
+    //     try{   
+    //         const response = await fetch(` https://api.themoviedb.org/3/trending/${mediaType}/week?api_key=${props.apiKey}`)
+    //         const data = await response.json();
+    //         listContext.exportedData(data.results)
+    //         props.setExpandedNav(!props.expandedNav)
+    //         props.setMediaNav('root')
+    //     }catch(err) {
+    //         console.log(err)
+    //     }
+    // }
+    // const queryMediaBySelectedGenre = async (e, mediaType, voteCount, page) => {
+    //     // setPageNavIO(!pageNavIO)
+    //     let test = `https://api.themoviedb.org/3/discover/${mediaType}?api_key=${props.apiKey}&language=en-US&sort_by=vote_average.desc&vote_count.gte=${voteCount}&with_genres=${e.value}&include_adult=false&include_video=false&page=${page}&watch_region=US`
+    //     try {
+    //         const response = await fetch(test)
+    //         const data = await response.json();
+    //         console.log(data)
+    //         console.log(test)
+    //         listContext.exportedData(data.results)
+    //         props.setExpandedNav(!props.expandedNav)
+    //         setMediaNav('root')
+    //     }catch(err) {
+    //         console.log(err)
+    //     }
+    // }
+    // const queryTopMediaAllGenres = async (mediaType, voteCount) => {
+    //     try {
+    //         const response = await fetch(`https://api.themoviedb.org/3/discover/${mediaType}?api_key=${props.apiKey}&language=en-US&sort_by=vote_average.desc&vote_count.gte=${voteCount}&page=1&timezone=America%2FTexas&include_null_first_air_dates=false`)
+    //         const data = await response.json();
+    //         listContext.exportedData(data.results)
+    //         props.setExpandedNav(!props.expandedNav)
+    //         props.setMediaNav('root')
+    //     }catch(err) {
+    //         console.log(err)
+    //     }
+    // }
 
-    const callApiForTvGenre = async (e) => {
-        setMediaNav('topTvShows')
-        try{
-            const responseGenre = await fetch(`https://api.themoviedb.org/3/genre/tv/list?api_key=${props.apiKey}&language=en-US`)
-            const dataGenre = await responseGenre.json();
-            const genreListQuery = dataGenre.genres;
-            setTvGenreList(genreListQuery);
-        }catch(err) {
-            console.log(err)
-        }
-    }
+    // const callApiForTvGenre = async (e) => {
+    //     props.setMediaNav('topTvShows')
+    //     try{
+    //         const responseGenre = await fetch(`https://api.themoviedb.org/3/genre/tv/list?api_key=${props.apiKey}&language=en-US`)
+    //         const dataGenre = await responseGenre.json();
+    //         const genreListQuery = dataGenre.genres;
+    //         setTvGenreList(genreListQuery);
+    //     }catch(err) {
+    //         console.log(err)
+    //     }
+    // }
     let rendered = null
-        if(mediaNav === 'root') {
+        if(props.mediaNav === 'root') {
             rendered = (
                 <div className={styles.navRouteWrapper}>
                 <div>
                                 {/*gets genreList as props from footer*/}
-                    <button onClick={() => setMediaNav('topMovies')}>Top Movies</button>
-                    <button onClick={() => queryTrendingMedia('movie')}>Trending Movies</button>
+                    <button onClick={() => props.setMediaNav('topMovies')}>Top Movies</button>
+                    <button onClick={() => props.queryTrendingMedia('movie')}>Trending Movies</button>
                 </div>
                 <div>
-                    <button onClick={callApiForTvGenre}>Top TV Shows</button>
-                    <button onClick={() => queryTrendingMedia('tv')}>Trending TV Shows</button>
+                    <button onClick={props.callApiForTvGenre}>Top TV Shows</button>
+                    <button onClick={() => props.queryTrendingMedia('tv')}>Trending TV Shows</button>
                 </div>
             </div>
             )
-        } else if(mediaNav === 'topMovies') {
+        } else if(props.mediaNav === 'topMovies') {
             rendered = (
             <div className={styles.movieNavWrapper}>
                 <div>
@@ -110,16 +105,16 @@ const NavSearch = props => {
                     className={styles.dropDownMain} 
                     controlClassName={styles.dropDownControl}
                     options={options} label={options.label} value={options.value}
-                    onChange={(e) => queryMediaBySelectedGenre(e, 'movie', '500')} 
+                    onChange={(e) => props.queryMediaBySelectedGenre(e, 'movie', '500')} 
                     placeholder='Select a genre' />  
                </div>
                 <div>
                     <h3>Top movies all genres</h3>
-                    <button onClick={() => queryTopMediaAllGenres( 'movie', '4000')}>Search All</button>
+                    <button onClick={() => props.queryTopMediaAllGenres( 'movie', '4000')}>Search All</button>
                 </div>
            </div> 
             )
-        } else if(mediaNav === 'topTvShows') {
+        } else if(props.mediaNav === 'topTvShows') {
             rendered = (
                 <div className={styles.movieNavWrapper}>
                 <div>
@@ -128,12 +123,12 @@ const NavSearch = props => {
                     className={styles.dropDownMain} 
                     controlClassName={styles.dropDownControl}
                     options={tvGenreListNames} label={tvGenreListNames.label} value={tvGenreListNames.value}
-                    onChange={(e) => queryMediaBySelectedGenre(e, 'tv', '100', '1' )} 
+                    onChange={(e) => props.queryMediaBySelectedGenre(e, 'tv', '100', '1' )} 
                     placeholder='Select a genre' />  
                </div>
                 <div>
                     <h3>Top TV all genres</h3>
-                    <button onClick={() => queryTopMediaAllGenres('tv', '5000')}>Search All</button>
+                    <button onClick={() => props.queryTopMediaAllGenres('tv', '5000')}>Search All</button>
                 </div>
             </div>
             )
@@ -141,9 +136,9 @@ const NavSearch = props => {
 
     return(
         <div style={expandedStyle} className={styles.mainWrapper}>
-            <div></div>
+            
            {rendered}
-           <div></div>
+           
         </div>
     )
 }
