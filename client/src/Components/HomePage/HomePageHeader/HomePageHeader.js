82bd0988091243bdb81f6d02b45efe6f77947dfa
Manuel Guzman <m_guzman74@yahoo.com>
2021-03-28 20:22:36 -0600

    removed subheader

--- client/src/Components/HomePage/HomePageHeader/HomePageHeader.js
+++ client/src/Components/HomePage/HomePageHeader/HomePageHeader.js
@@ -2,20 +2,11 @@ import React from 'react';
 import styles from './HomePageHeader.module.css';
 
 const HomePageHeader = props => {
-  const mq = window.matchMedia("(min-width: 768px)");
-  let subHeader = null
-  if(mq.matches) {
-    subHeader = 'Top 20 this week'
-  }
   return(
     <div className={styles.headerWrapper}>
       <div>
         <h2>MovieKnight</h2>
-      </div>
-      <div>
-      {subHeader}
-      </div>
-      
+      </div>      
     </div>
   )
 }
