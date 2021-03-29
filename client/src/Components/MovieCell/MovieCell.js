5bf54bf7421d5fbe018feffd0baea5e79612fdc8
Manuel Guzman <m_guzman74@yahoo.com>
2021-03-28 19:58:43 -0600

    added miscinfo to desktop view

--- client/src/Components/MovieCell/MovieCell.js
+++ client/src/Components/MovieCell/MovieCell.js
@@ -63,6 +63,8 @@ const MovieCell = (props) => {
           </div>
           {releaseRender}
           <div className={styles.largeBio}>{props.bio}</div>
+          <MiscInfo streamingServices={streamingServices}
+            mediaType={props.mediaType} />
         </div>
       </React.Fragment>
     );
