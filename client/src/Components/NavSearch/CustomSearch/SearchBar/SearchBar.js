1a8bcc787f232a6e230890a104c7dc69fd53277d
Manuel Guzman <m_guzman74@yahoo.com>
2021-04-20 01:43:39 -0500

    css fixes, search bar focuses on render

--- client/src/Components/NavSearch/CustomSearch/SearchBar/SearchBar.js
+++ client/src/Components/NavSearch/CustomSearch/SearchBar/SearchBar.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './SearchBar.module.css';
 
 const SearchBar = props => {
-  
+  const inputRef = useRef();
   const handleKeyPress = event => {
     if(event.key === 'Enter') {
       props.clickedSearchHandler(event);
     }
   }
+  useEffect(() => {
+    inputRef.current.focus()
+  })
   return(
     <div className={styles.mainWrapper}>
       <form>
-        <input type="search" 
+        <input type="search" ref={inputRef}
         value={props.searchData} 
         onChange={e => props.setSearchData(e.target.value)}
         onKeyPress={handleKeyPress} />
--- client/src/Components/NavSearch/MediaTypeBtn/MediaTypeBtn.js
+++ client/src/Components/NavSearch/MediaTypeBtn/MediaTypeBtn.js
@@ -5,7 +5,7 @@ const MediaTypeBtn = props => (
     <div >
       <h3>Search for...</h3>
       <button className={styles.mediaBtn} 
-      onClick={props.clickedBtn} value='movie'>Movie</button>
+      onClick={props.clickedBtn} value='movie'>Movies</button>
       <button className={styles.mediaBtn} 
       onClick={props.clickedBtn} value='tv'>TV</button>
     </div>
