1c85d888828c1cd7f0fea69b6e2d4a2f0c1d5701
Manuel Guzman <m_guzman74@yahoo.com>
2021-05-15 01:02:12 -0500

    troubleshooting server middleware

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(__dirname, 'build'));
 
   app.get('/*', () => (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    console.log(req)
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 
